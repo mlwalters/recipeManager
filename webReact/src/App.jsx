9fb1096fefea5798a2d267883d57dda965b5fe12
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2021-12-16 11:37:30 -0600

    changed css framework from bootstrap to material ui

--- webReact/src/App.jsx
+++ webReact/src/App.jsx
@@ -2,7 +2,6 @@ import './App.scss';
 import React from 'react';
 // import { Route, Routes } from 'react-router';
 import Recipe from './components/Recipe';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- webReact/src/components/Recipe.jsx
+++ webReact/src/components/Recipe.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {
-  Container, Card, Button, Row, Col,
-} from 'react-bootstrap';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const Recipe = () => {
   const [details, setDetails] = useState({});
@@ -28,55 +32,62 @@ const Recipe = () => {
   }
 
   return (
-    <Container fluid>
-      <Row>
-        <Col sm={2}>
-          <ul>
-            <li>Meal Planner</li>
-            <li>Grocery List</li>
-            <li>Meals Last Cooked</li>
-            <li>Multiple Timer</li>
-            <li>Unit Converter</li>
-          </ul>
-        </Col>
-        <Col>
-          <div>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="https://images.unsplash.com/photo-1621955511667-e2c316e4575d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hlZXNlY2FrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60" />
-              <Card.Body>
-                <Card.Title>{details.name}</Card.Title>
-                <Card.Text>{details.description}</Card.Text>
-                <Card.Text>
-                  Serving size:
-                  {details.servingSize}
-                </Card.Text>
-                <Button variant="primary">Go to Recipe</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-    // <Card style={{ width: '18rem' }}>
-    //   <Card.Img variant="top" src="https://images.unsplash.com/photo-1621955511667-e2c316e4575d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hlZXNlY2FrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60" />
-    //   <Card.Body>
-    //     <Card.Title>{details.name}</Card.Title>
-    //     <Card.Text>{details.description}</Card.Text>
-    //     <Card.Text>
-    //       Serving size:
-    //       {details.servingSize}
-    //     </Card.Text>
-    //     <Button variant="primary">Go to Recipe</Button>
-    //   </Card.Body>
-    // </Card>
-    // <div>
-    //   <h2>{details.name}</h2>
-    //   <div>
-    //     <p>{details.description}</p>
-    //     <p>{details.servingSize}</p>
-    //     <p>{details.notes}</p>
-    //   </div>
-    // </div>
+    <Grid container>
+      <Grid xs={2}>
+        <Typography variant="h6" color="primary">
+          Meal Planner
+        </Typography>
+      </Grid>
+      <Grid xs={10}>
+        <Grid item>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              width="100"
+              image="https://images.unsplash.com/photo-1621955511667-e2c316e4575d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hlZXNlY2FrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+              alt="strawberry cheesecake"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {details.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {details.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" size="small" color="secondary">View Recipe</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              width="100"
+              image="https://images.unsplash.com/photo-1621955511667-e2c316e4575d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hlZXNlY2FrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+              alt="lentil soup"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {details.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {details.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" size="small" color="secondary">View Recipe</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
