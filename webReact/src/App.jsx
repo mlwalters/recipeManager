e8d89e67e996f96f9b3e24281a9f9808587f4e2f
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2021-12-29 13:36:07 -0600

    Fixed routing error and docker issue

--- webReact/src/App.jsx
+++ webReact/src/App.jsx
@@ -1,24 +1,16 @@
 import './App.scss';
 import React from 'react';
-import { Routes, Route } from 'react-router';
-// import { Route } from 'react-router';
-// import { Switch } from 'react-router-dom';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </Router>
-      {/* <Dashboard /> */}
+      <Routes>
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
