1c181c05429da6cdbf7ada500a779de26b9c72f0
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-01-19 09:15:03 -0600

    11 - Updated item model and ingredientResponse

--- recipeAPI/api/ViewModels/RecipeResponse.cs
+++ recipeAPI/api/ViewModels/RecipeResponse.cs
@@ -85,8 +85,6 @@ namespace api.Models
         public int Id { get; set; }
         public string Amount { get; set; }
         public string Name { get; set; }
-        // public int ItemId { get; set; }
-        // public virtual Item Item { get; set; }
     }
     public class AddIngredient
     {
