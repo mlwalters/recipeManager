ea18a804b8ca0690a8bbb7191fcc2966a2e22339
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-01-19 15:45:14 -0600

    11 - Removed unneeded property in ingredient model

--- recipeAPI/api/Models/Ingredient.cs
+++ recipeAPI/api/Models/Ingredient.cs
@@ -7,18 +7,10 @@ namespace api.Models
     {
         public int Id { get; set; }
         public string Amount { get; set; }
-        public string Name { get; set; }
         public int ItemId { get; set; }
         public int RecipeId { get; set; }
         public virtual Recipe Recipe { get; set; }
         public virtual Item Item { get; set; }
 
-        public Ingredient(int id, string amount, Item item)
-        {
-            Id = id;
-            Amount = amount;
-            Name = Item.ItemName;
-        }
-        public Ingredient(){}
     }
 }
