18d516a0987418fb0542aef28e1eb18502f71852
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-01-19 18:09:08 -0600

    15 - Added category controller for the select form in the front-end

---
+++ recipeAPI/api/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<RecipeController> _logger;
+
+        public CategoryController(AppDbContext context, ILogger<RecipeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var categories = await _context.Categories.ToListAsync();
+
+                return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical($"SQL Read error. It is likely that there is no database connection established. ${e.Message}");
+                throw;
+            }
+        }
+    }
+}
