f610ead35a60027962ed88b1f9403e2cdd9640df
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-01 22:20:57 -0600

    17 - Added navbar component

---
+++ webReact/src/components/sharedComponents/NavBar.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NavBar from './NavBar';
+
+test('renders navigation bar on the home page', async () => {
+  render(
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>,
+  );
+  expect(await screen.findByText('LOGO')).toBeInTheDocument();
+  expect(await screen.findByText('Recipes')).toBeInTheDocument();
+  expect(await screen.findByText('Favorites')).toBeInTheDocument();
+  expect(await screen.findByText('Meal Planner')).toBeInTheDocument();
+});
