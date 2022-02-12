8652073d3f87344572c8a8871bcd3882bfdc4b77
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-11 19:24:11 -0600

    17 - Cleaned up code and fixed homepage tests

--- web/src/components/HomePage.test.jsx
+++ web/src/components/HomePage.test.jsx
@@ -1,13 +1,100 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import HomePage from './HomePage';
 
-test('renders recipe dashboard on the home page', async () => {
+const recipeCards = [{
+  id: 1,
+  name: 'Strawberry Cheesecake',
+  description: 'A light-yet-rich cheesecake, creamynot dense-creamy like New York cheesecake.',
+  servingSize: 12,
+  category: 'Dessert',
+  instructions: [
+    {
+      id: 1,
+      step: 'Preheat oven to 160C/320F (standard) or 140C/295F (fan/convection).',
+      stepNumber: 1,
+    },
+    {
+      id: 2,
+      step: 'Butter and line the side of the pan.',
+      stepNumber: 2,
+    },
+  ],
+  ingredients: [
+    {
+      id: 1,
+      amount: '1/2 tsp',
+      name: 'salt',
+    },
+    {
+      id: 2,
+      amount: '1 cup',
+      name: 'sugar',
+    },
+  ],
+},
+{
+  id: 2,
+  name: 'Lentil Soup',
+  description: 'The touch of spices with lemon really lifts this soup to the next level.',
+  servingSize: 6,
+  category: 'Soup',
+  instructions: [
+    {
+      id: 3,
+      step: 'Add garlic and onion, cook for 2 minutes.',
+      stepNumber: 1,
+    },
+    {
+      id: 4,
+      step: 'Add celery and carrot.',
+      stepNumber: 2,
+    },
+  ],
+  ingredients: [
+    {
+      id: 3,
+      amount: '3 cups',
+      name: 'brown lentils',
+    },
+    {
+      id: 4,
+      amount: '1 tsp',
+      name: 'salt',
+    },
+  ],
+}];
+
+const categories = [
+  {
+    id: 0,
+    name: 'Seafood',
+  },
+  {
+    id: 1,
+    name: 'Beef',
+  },
+  {
+    id: 2,
+    name: 'Pork',
+  },
+  {
+    id: 3,
+    name: 'Poultry',
+  },
+];
+
+test('renders add recipe button on the home page', async () => {
+  const mockApi = new MockAdapter(axios);
+  mockApi.onGet(`${process.env.REACT_APP_BASE_API}/api/Recipe`).reply(200, recipeCards);
+  mockApi.onGet(`${process.env.REACT_APP_BASE_API}/api/Category`).reply(200, categories);
   render(
     <BrowserRouter>
       <HomePage />
     </BrowserRouter>,
   );
-  expect(await screen.findByText('LOGO')).toBeInTheDocument();
+  expect(await screen.findByRole('button', { name: /add recipe/i }));
 });
--- web/src/components/sharedComponents/NavBar.test.jsx
+++ web/src/components/sharedComponents/NavBar.test.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-// import axios from 'axios';
-// import MockAdapter from 'axios-mock-adapter';
 import NavBar from './NavBar';
 
 test('renders navigation bar on the home page', async () => {
