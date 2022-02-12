53b8a533eae76d2fa9c346de2cb6a3fec95eed07
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-11 18:36:20 -0600

    Changed web folder name

--- webReact/src/components/sharedComponents/LoadingDisplay.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-
-const LoadingDisplay = () => (<Typography variant="h3">Loading...</Typography>);
-
-export default LoadingDisplay;
