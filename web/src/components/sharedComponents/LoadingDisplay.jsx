9f47c2cf9382a5447b29a9b0883ec05ea65f122b
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-18 23:19:19 -0600

    10 - Added a rotating logo in the loading display

--- web/src/components/sharedComponents/LoadingDisplay.jsx
+++ web/src/components/sharedComponents/LoadingDisplay.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import rotatingLogo from '../../assets/shared-images/loadingLogo.jpg';
+import './LoadingDisplay.css';
 
-const LoadingDisplay = () => (<Typography component="span" variant="body2">Loading...</Typography>);
-
+const LoadingDisplay = () => (
+  <>
+    <Typography component="span" variant="h6">Loading...</Typography>
+    <img className="rotate" src={rotatingLogo} alt="loading display" height="120px" m="10" />
+  </>
+);
 export default LoadingDisplay;
