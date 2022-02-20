391664dfa0c7d8ce89f51ad66c4839a47562ce69
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-19 21:31:47 -0600

    15 - Changed some form styling

--- web/src/components/AddForm.jsx
+++ web/src/components/AddForm.jsx
@@ -20,6 +20,7 @@ import {
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import Stack from '@mui/material/Stack';
 
 // TODO : image url
 const initialValues = {
@@ -99,205 +100,189 @@ const AddForm = () => {
           errors, isValid, touched, dirty, values,
         }) => (
           <Form>
-            <Field
-              label="Recipe name"
-              name="name"
-              type="text"
-              as={TextField}
-              required
-              data-testid="Recipe name"
-              variant="outlined"
-              size="small"
-              error={Boolean(errors.name) && Boolean(touched.name)}
-              helperText={Boolean(touched.name) && errors.name}
-            />
-            <Box height={14} />
+            <Stack spacing={2}>
+              <Field
+                label="Recipe name"
+                name="name"
+                type="text"
+                as={TextField}
+                required
+                data-testid="Recipe name"
+                variant="outlined"
+                size="small"
+                error={Boolean(errors.name) && Boolean(touched.name)}
+                helperText={Boolean(touched.name) && errors.name}
+              />
 
-            <Field
-              label="Description"
-              name="description"
-              type="description"
-              as={TextField}
-              variant="outlined"
-              size="small"
-              multiline
-              rows={2}
-              data-testid="Description"
-              error={Boolean(errors.description) && Boolean(touched.description)}
-              helperText={Boolean(touched.description) && errors.description}
-            />
-            <Box height={14} />
+              <Field
+                label="Description"
+                name="description"
+                type="description"
+                as={TextField}
+                variant="outlined"
+                size="small"
+                multiline
+                rows={2}
+                data-testid="Description"
+                error={Boolean(errors.description) && Boolean(touched.description)}
+                helperText={Boolean(touched.description) && errors.description}
+              />
 
-            <Field
-              label="Image URL"
-              name="imageUrl"
-              type="imageUrl"
-              as={TextField}
-              variant="outlined"
-              size="small"
-              multiline
-              rows={2}
-              data-testid="Image"
-              error={Boolean(errors.imageUrl) && Boolean(touched.imageUrl)}
-              helperText={Boolean(touched.imageUrl) && errors.imageUrl}
-            />
-            <Box height={14} />
+              <Field
+                label="Image URL"
+                name="imageUrl"
+                type="imageUrl"
+                as={TextField}
+                variant="outlined"
+                size="small"
+                multiline
+                rows={2}
+                data-testid="Image"
+                error={Boolean(errors.imageUrl) && Boolean(touched.imageUrl)}
+                helperText={Boolean(touched.imageUrl) && errors.imageUrl}
+              />
 
-            <Field
-              label="Category"
-              name="category"
-              type="category"
-              as={TextField}
-              select
-              fullWidth
-              variant="outlined"
-              size="small"
-              error={Boolean(errors.category) && Boolean(touched.category)}
-              helperText={Boolean(touched.category) && errors.category}
-            >
-              {categories.map((option) => (
-                <Field
-                  as={MenuItem}
-                  key={option.value}
-                  value={option.id}
-                >
-                  {option.name}
-                </Field>
-              ))}
-            </Field>
-            <Box height={14} />
+              <Field
+                label="Category"
+                name="category"
+                type="category"
+                as={TextField}
+                select
+                fullWidth
+                variant="outlined"
+                size="small"
+                error={Boolean(errors.category) && Boolean(touched.category)}
+                helperText={Boolean(touched.category) && errors.category}
+              >
+                {categories.map((option) => (
+                  <Field
+                    as={MenuItem}
+                    key={option.value}
+                    value={option.id}
+                  >
+                    {option.name}
+                  </Field>
+                ))}
+              </Field>
 
-            <Field
-              label="Serving Size"
-              variant="outlined"
-              size="small"
-              type="number"
-              as={TextField}
-              name="servingSize"
-              data-testid="Serving Size"
-              error={Boolean(errors.servingSize) && Boolean(touched.servingSize)}
-              helperText={Boolean(touched.servingSize) && errors.servingSize}
-            />
-            <Box height={14} />
+              <Field
+                label="Serving Size"
+                variant="outlined"
+                size="small"
+                type="number"
+                as={TextField}
+                name="servingSize"
+                data-testid="Serving Size"
+                error={Boolean(errors.servingSize) && Boolean(touched.servingSize)}
+                helperText={Boolean(touched.servingSize) && errors.servingSize}
+              />
 
-            <FormGroup>
-              <FormControl>
-                <FormLabel>Ingredients</FormLabel>
-                <Box height={14} />
-                <FieldArray
-                  name="ingredients"
-                  render={(arrayHelpers) => (
-                    <div>
-                      {values.ingredients && values.ingredients.length > 0 ? (
-                        values.ingredients.map((ingredient, index) => (
+              <FormGroup>
+                <FormControl>
+                  <FormLabel>Ingredients</FormLabel>
+
+                  <FieldArray
+                    name="ingredients"
+                    render={(arrayHelpers) => (
+                      <div>
+                        {values.ingredients && values.ingredients.length > 0 ? (
+                          values.ingredients.map((ingredient, index) => (
                           // eslint-disable-next-line react/no-array-index-key
-                          <div key={index}>
-                            <Field
-                              name={`ingredients.${index}.amount`}
-                              label="Amount"
-                              as={TextField}
-                              variant="outlined"
-                              size="small"
-                              // error={Boolean(errors.ingredients.amount)
-                              //   && Boolean(errors.ingredients.amount)}
-                              // helperText={Boolean(touched.amount)
-                              //   && errors.amount}
-                            />
-                            <Field
-                              name={`ingredients.${index}.item`}
-                              label="Ingredient"
-                              as={TextField}
-                              variant="outlined"
-                              size="small"
-                              // error={Boolean(errors.ingredients.item)
-                              //   && Boolean(errors.ingredients.item)}
-                              // helperText={Boolean(touched.item)
-                              //   && errors.item}
-                            />
-                            {index ? (
-                              <RemoveIcon
-                                disabled={index.length === 1}
-                                onClick={() => arrayHelpers.remove(index)}
+                            <div key={index}>
+                              <Field
+                                name={`ingredients.${index}.amount`}
+                                label="Amount"
+                                as={TextField}
+                                variant="outlined"
+                                size="small"
                               />
-                            ) : null}
-                            <AddIcon
-                              onClick={() => arrayHelpers.insert(index + 1, '')}
-                            />
-                          </div>
-                        ))
-                      ) : null}
-                    </div>
-                  )}
-                />
-              </FormControl>
-            </FormGroup>
-            <Box height={14} />
+                              <Field
+                                name={`ingredients.${index}.item`}
+                                label="Ingredient"
+                                as={TextField}
+                                variant="outlined"
+                                size="small"
+                              />
+                              {index ? (
+                                <RemoveIcon
+                                  disabled={index.length === 1}
+                                  onClick={() => arrayHelpers.remove(index)}
+                                />
+                              ) : null}
+                              <AddIcon
+                                onClick={() => arrayHelpers.insert(index + 1, '')}
+                              />
+                            </div>
+                          ))
+                        ) : null}
+                      </div>
+                    )}
+                  />
+                </FormControl>
+              </FormGroup>
 
-            <FormGroup>
-              <FormControl>
-                <FormLabel>Instructions</FormLabel>
-                <Box height={14} />
-                <FieldArray
-                  name="instructions"
-                  render={(arrayHelpers) => (
-                    <div>
-                      {values.instructions && values.instructions.length > 0 ? (
-                        values.instructions.map((instruction, index) => (
+              <FormGroup>
+                <FormControl>
+                  <FormLabel>Instructions</FormLabel>
+
+                  <FieldArray
+                    name="instructions"
+                    render={(arrayHelpers) => (
+                      <div>
+                        {values.instructions && values.instructions.length > 0 ? (
+                          values.instructions.map((instruction, index) => (
                           // eslint-disable-next-line react/no-array-index-key
-                          <Box key={index} display="flex">
-                            <Typography variant="body2" color="text.secondary">{`Step ${index + 1}`}</Typography>
-                            <Field
-                              name={`instructions.${index}.step`}
-                              label="Step"
-                              as={TextField}
-                              variant="outlined"
-                              size="small"
-                              // error={Boolean(errors.step) && Boolean(touched.step)}
-                              // helperText={Boolean(touched.step) && errors.step}
-                            />
-                            {index ? (
-                              <RemoveIcon
-                                disabled={index.length === 1}
-                                onClick={() => arrayHelpers.remove(index)}
+                            <Box key={index} display="flex">
+                              <Typography variant="body2" color="text.secondary">{`Step ${index + 1}`}</Typography>
+                              <Field
+                                name={`instructions.${index}.step`}
+                                label="Step"
+                                as={TextField}
+                                variant="outlined"
+                                size="small"
                               />
-                            ) : null}
-                            <AddIcon
-                              onClick={() => arrayHelpers.insert(index + 1, { step: '' })}
-                            />
-                          </Box>
-                        ))
-                      ) : null}
-                    </div>
-                  )}
-                />
-              </FormControl>
-            </FormGroup>
-            <Box height={14} />
+                              {index ? (
+                                <RemoveIcon
+                                  disabled={index.length === 1}
+                                  onClick={() => arrayHelpers.remove(index)}
+                                />
+                              ) : null}
+                              <AddIcon
+                                onClick={() => arrayHelpers.insert(index + 1, { step: '' })}
+                              />
+                            </Box>
+                          ))
+                        ) : null}
+                      </div>
+                    )}
+                  />
+                </FormControl>
+              </FormGroup>
 
-            <Field
-              label="Notes"
-              variant="outlined"
-              size="small"
-              fullWidth
-              multiline
-              rows={5}
-              as={TextField}
-              name="notes"
-              data-testid="Serving Size"
-              error={Boolean(errors.notes) && Boolean(touched.notes)}
-              helperText={Boolean(touched.notes) && errors.notes}
-            />
-            <Box height={14} />
+              <Field
+                label="Notes"
+                variant="outlined"
+                size="small"
+                fullWidth
+                multiline
+                rows={5}
+                as={TextField}
+                name="notes"
+                data-testid="Serving Size"
+                error={Boolean(errors.notes) && Boolean(touched.notes)}
+                helperText={Boolean(touched.notes) && errors.notes}
+              />
 
-            <Button
-              type="submit"
-              variant="contained"
-              color="secondary"
-              size="large"
-              disabled={!isValid || !dirty}
-            >
-              Add Recipe
-            </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                size="large"
+                disabled={!isValid || !dirty}
+              >
+                Add Recipe
+              </Button>
+            </Stack>
           </Form>
         )}
       </Formik>
--- web/src/components/AddRecipePage.jsx
+++ web/src/components/AddRecipePage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import AddForm from './AddForm';
-import NavBar from './sharedComponents/NavBar';
+// import NavBar from './sharedComponents/NavBar';
 
 const AddRecipePage = () => (
-  <>
-    <NavBar />
-    <Container maxWidth="lg">
-      <AddForm />
-    </Container>
-  </>
+  // <>
+  //   <NavBar />
+  //   <Container maxWidth="lg">
+  //     <AddForm />
+  //   </Container>
+  // </>
+  <Container maxWidth="lg">
+    {/* <NavBar /> */}
+    <AddForm />
+  </Container>
 );
 
 export default AddRecipePage;
