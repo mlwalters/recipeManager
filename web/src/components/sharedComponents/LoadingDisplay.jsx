6cfb1ecedfbc467a228f91d5c503e3993629ec06
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-02-20 19:32:59 -0600

    10 - Changed global loading display from rotating logo to text

--- web/src/components/sharedComponents/LoadingDisplay.jsx
+++ web/src/components/sharedComponents/LoadingDisplay.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import rotatingLogo from '../../assets/shared-images/loadingLogo.jpg';
+// import rotatingLogo from '../../assets/shared-images/loadingLogo.jpg';
 import './LoadingDisplay.css';
+import { Box } from '@mui/material';
 
 const LoadingDisplay = () => (
-  <>
-    <Typography component="span" variant="h6">Loading...</Typography>
-    <img className="rotate" src={rotatingLogo} alt="loading display" height="120px" m="10" />
-  </>
+  <Box m={2} p={2}>
+    <Typography component="span" variant="h3">Loading...</Typography>
+    {/* <img className="rotate" src={rotatingLogo}
+    alt="loading display" height="120px" m="10" /> */}
+  </Box>
 );
 export default LoadingDisplay;
