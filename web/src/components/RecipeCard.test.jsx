90caf3731abb5f70abcb11c54c0c79479326e8ce
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-03-11 09:58:50 -0600

    WIP writing test for the reusable component recipe card

---
+++ web/src/components/RecipeCard.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import axios from 'axios';
+// import { BrowserRouter } from 'react-router-dom';
+// import MockAdapter from 'axios-mock-adapter';
+import { render, screen } from '@testing-library/react';
+import RecipeCard from './RecipeCard';
+
+const userEmail = 'google@yahoo.com';
+jest.mock('@auth0/auth0-react', () => ({
+  useAuth0: () => ({
+    user: {
+      email: userEmail,
+    },
+  }),
+}));
+
+const testRecipeCard = {
+  title: 'Roasted Chicken',
+  category: 'Poultry',
+  description: 'Juicy and tasty',
+};
+
+test('renders card header', async () => {
+  render(<RecipeCard />);
+  expect(await screen.findByText(testRecipeCard.title)).toBeInTheDocument();
+});
