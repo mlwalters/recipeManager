3e00a7ede1d653169d7bfcc43228c73d6c8cbdda
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-11-18 19:26:39 -0600

    47 - Added a new component for not found error messages

---
+++ web/src/components/error-msgs/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import { Box, Typography } from '@mui/material';
+import notfound from '../../assets/shared-images/notfound.jpg';
+
+const NotFound = () => (
+  <Container maxWidth="md">
+    <Box sx={{
+      display: 'flex', alignItems: 'center', width: '100%', marginTop: 5, marginBottom: 5,
+    }}
+    >
+      <img src={notfound} alt="Error message: Oops, there was an error." width="500" />
+      <Typography variant="h5">Oops, there was an error.</Typography>
+    </Box>
+  </Container>
+);
+
+export default NotFound;
