8d1f020f29490c5ee659303b7f960ca90b9dcbc2
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-12-19 14:43:21 -0600

    48 - Updated error handling when fetching categories

--- web/src/views/dashboard/HomePage.jsx
+++ web/src/views/dashboard/HomePage.jsx
@@ -3,16 +3,19 @@ import React, { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-// import Fab from '@mui/material/Fab';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import Fab from '@mui/material/Fab';
+// import Typography from '@mui/material/Typography';
 import ToggleButtonView from '../../components/ToggleButtonView';
 import Toast, { variants } from '../../components/toast/Toast';
 import AddRecipeForm from '../../components/forms/AddRecipeForm';
+import NotFound from '../../components/error-msgs/NotFound';
+// import BackToHomeBtn from '../../components/navigation/back-to-home/BackToHomeBtn';
 
 const HomePage = () => {
   const [categories, setCategories] = useState([{}]);
   const [submitError, setSubmitError] = useState(null);
+  const [fetchCategoryError, setFetchCategoryError] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
   const [toastVariant, setToastVariant] = useState(variants.info);
@@ -20,8 +23,14 @@ const HomePage = () => {
 
   useEffect(() => {
     const fetchCategories = async () => {
-      const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Category`);
-      setCategories(data);
+      try {
+        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Category`);
+        setCategories(data);
+      } catch (err) {
+        setFetchCategoryError(err);
+        setToastMessage('Oops! There was an error, try again');
+        setToastVariant(variants.error);
+      }
     };
     fetchCategories();
   }, []);
@@ -42,14 +51,27 @@ const HomePage = () => {
       setToastVariant(variants.success);
     } catch (err) {
       setSubmitError(err);
+      // setToastMessage('Oops! Recipe submission failed, try again');
+      // setToastVariant(variants.error);
     }
   };
 
   if (submitError) {
     return (
-      <Typography component="p" variant="h6">Oops! Recipe submission failed.</Typography>
+      <Container maxWidth="lg">
+        <NotFound />
+      </Container>
     );
   }
+
+  if (fetchCategoryError) {
+    return (
+      <Container maxWidth="lg">
+        <NotFound />
+      </Container>
+    );
+  }
+
   return (
     <Container maxWidth="lg">
       <Box sx={{
