a70033a479ea0e269250224e34d97c1fe3da1c0a
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-12-19 19:35:02 -0600

    49 - Moved add recipe button inside Toggle Button View component

--- web/src/components/ToggleButtonView.jsx
+++ web/src/components/ToggleButtonView.jsx
@@ -1,15 +1,83 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { Paper, Box, Typography } from '@mui/material';
+import {
+  Paper, Box, Typography, Container,
+} from '@mui/material';
+import Button from '@mui/material/Button';
+import { useAuth0 } from '@auth0/auth0-react';
+import Toast, { variants } from './toast/Toast';
+import AddRecipeForm from './forms/AddRecipeForm';
+import NotFound from './error-msgs/NotFound';
 import ListView from './recipe-list/list-view/ListView';
-// import CardView from './recipe-list/card-view/CardView';
 import RecipeCard from './new-recipe-card/RecipeCard';
 
 export default function ToggleButtonView() {
   const [view, setView] = React.useState('card');
+  const [categories, setCategories] = useState([{}]);
+  const [submitError, setSubmitError] = useState(null);
+  const [fetchCategoryError, setFetchCategoryError] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [toastVariant, setToastVariant] = useState(variants.info);
+
+  const { user } = useAuth0();
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Category`);
+        setCategories(data);
+      } catch (err) {
+        setFetchCategoryError(err);
+        setToastMessage('Oops! There was an error, try again');
+        setToastVariant(variants.error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSubmitError(null);
+  };
+
+  const handleSubmitModal = async (values) => {
+    const request = values;
+    request.userEmail = user.email;
+    try {
+      // const { data } =
+      await axios.post(`${process.env.REACT_APP_BASE_API}/api/Recipe`, request);
+      // setRecipes(data);
+      setSubmitError(null);
+      handleCloseModal();
+      setToastMessage('New recipe added');
+      setToastVariant(variants.success);
+    } catch (err) {
+      setSubmitError(err);
+      // setToastMessage('Oops! Recipe submission failed, try again');
+      // setToastVariant(variants.error);
+    }
+  };
+
+  if (submitError) {
+    return (
+      <Container maxWidth="lg">
+        <NotFound />
+      </Container>
+    );
+  }
+
+  if (fetchCategoryError) {
+    return (
+      <Container maxWidth="lg">
+        <NotFound />
+      </Container>
+    );
+  }
 
   return (
     <Paper sx={{ padding: 2 }} elevation={0}>
@@ -29,8 +97,27 @@ export default function ToggleButtonView() {
         </ToggleButton>
         <Box sx={{ paddingTop: 1, paddingLeft: 1.5 }}>
           <Typography variant="h5">View All</Typography>
+          <Box sx={{
+            padding: 2, margin: 1,
+          }}
+          >
+            <Button variant="contained" onClick={() => setIsModalOpen(true)}>Add Recipe</Button>
+          </Box>
         </Box>
       </ToggleButtonGroup>
+      {isModalOpen && (
+        <AddRecipeForm
+          categories={categories}
+          saveError={submitError}
+          onFormSubmit={handleSubmitModal}
+          handleClose={handleCloseModal}
+        />
+      )}
+      <Toast
+        onClose={() => setToastMessage('')}
+        message={toastMessage}
+        variant={toastVariant}
+      />
       {view === 'list' ? (
         <Box>
           <ListView />
--- web/src/views/dashboard/HomePage.jsx
+++ web/src/views/dashboard/HomePage.jsx
@@ -1,100 +1,11 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react';
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-// import Fab from '@mui/material/Fab';
-// import Typography from '@mui/material/Typography';
 import ToggleButtonView from '../../components/ToggleButtonView';
-import Toast, { variants } from '../../components/toast/Toast';
-import AddRecipeForm from '../../components/forms/AddRecipeForm';
-import NotFound from '../../components/error-msgs/NotFound';
-// import BackToHomeBtn from '../../components/navigation/back-to-home/BackToHomeBtn';
 
-const HomePage = () => {
-  const [categories, setCategories] = useState([{}]);
-  const [submitError, setSubmitError] = useState(null);
-  const [fetchCategoryError, setFetchCategoryError] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [toastMessage, setToastMessage] = useState('');
-  const [toastVariant, setToastVariant] = useState(variants.info);
-  const { user } = useAuth0();
+const HomePage = () => (
+  <Container maxWidth="lg">
+    <ToggleButtonView />
+  </Container>
+);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Category`);
-        setCategories(data);
-      } catch (err) {
-        setFetchCategoryError(err);
-        setToastMessage('Oops! There was an error, try again');
-        setToastVariant(variants.error);
-      }
-    };
-    fetchCategories();
-  }, []);
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setSubmitError(null);
-  };
-
-  const handleSubmitModal = async (values) => {
-    const request = values;
-    request.userEmail = user.email;
-    try {
-      await axios.post(`${process.env.REACT_APP_BASE_API}/api/Recipe`, request);
-      setSubmitError(null);
-      handleCloseModal();
-      setToastMessage('New recipe added');
-      setToastVariant(variants.success);
-    } catch (err) {
-      setSubmitError(err);
-      // setToastMessage('Oops! Recipe submission failed, try again');
-      // setToastVariant(variants.error);
-    }
-  };
-
-  if (submitError) {
-    return (
-      <Container maxWidth="lg">
-        <NotFound />
-      </Container>
-    );
-  }
-
-  if (fetchCategoryError) {
-    return (
-      <Container maxWidth="lg">
-        <NotFound />
-      </Container>
-    );
-  }
-
-  return (
-    <Container maxWidth="lg">
-      <Box sx={{
-        padding: 2, margin: 1,
-      }}
-      >
-        <Button variant="contained" onClick={() => setIsModalOpen(true)}>Add Recipe</Button>
-      </Box>
-      <ToggleButtonView />
-      {isModalOpen && (
-        <AddRecipeForm
-          categories={categories}
-          saveError={submitError}
-          onSubmit={handleSubmitModal}
-          handleClose={handleCloseModal}
-        />
-      )}
-      <Toast
-        onClose={() => setToastMessage('')}
-        message={toastMessage}
-        variant={toastVariant}
-      />
-    </Container>
-  );
-};
 export default HomePage;
