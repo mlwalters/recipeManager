ff5e95e53033400f97796fbb1bac26b368075f0a
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2022-12-21 17:49:34 -0600

    49 - Refactored cards and list view

--- web/src/components/new-recipe-card/RecipeCard.jsx
+++ web/src/components/new-recipe-card/RecipeCard.jsx
@@ -1,7 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
@@ -13,178 +12,124 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Tooltip from '@mui/material/Tooltip';
 import { red } from '@mui/material/colors';
 import DeleteDialogBox from '../DeleteDialogBox';
-import LoadingDisplay from '../loading-display/LoadingDisplay';
 import SwitchImageCard from '../SwitchImageCard';
 import './RecipeCard.css';
-import NotFound from '../error-msgs/NotFound';
-import Toast, { variants } from '../toast/Toast';
-// import { useToggle } from '../useToggle';
 
-const RecipeCard = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [loadingState, setLoadingState] = useState(true);
-  const [fetchError, setFetchError] = useState(null);
-  const [open, setOpen] = useState(false);
-  const [favoriteToggle, setFavoriteToggle] = useState(false);
-  const [toastMessage, setToastMessage] = useState('');
-  const [toastVariant, setToastVariant] = useState(variants.info);
-  const { user } = useAuth0();
-  // const { status: favoriteToggle, toggleStatus: setFavoriteToggle } = useToggle();
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => setOpen(false);
-
-  const handleDelete = (id) => {
-    setOpen(false);
-    const deleteData = async () => {
-      try {
-        const { data } = await axios.delete(`${process.env.REACT_APP_BASE_API}/api/Recipe/${id}`);
-        setRecipes(data);
-        setToastMessage('Recipe has been deleted');
-        setToastVariant(variants.info);
-      } catch (err) {
-        setToastMessage('Oops! Could not delete recipe, try again');
-        setToastVariant(variants.error);
-      }
-    };
-    deleteData();
-  };
-
-  const handleClickFavorite = async (id) => {
-    const recipeToUpdate = recipes.find((recipe) => recipe.id === id);
-    try {
-      setFavoriteToggle((toggle) => !toggle);
-      const request = { ...recipeToUpdate, favorite: favoriteToggle, userEmail: user.email };
-      const { data } = await axios.put(`${process.env.REACT_APP_BASE_API}/api/Recipe/${id}`, request);
-      setRecipes(data);
-      if (!recipeToUpdate.favorite) {
-        setToastMessage('Recipe saved in Favorites');
-        setToastVariant(variants.success);
-      } else {
-        setToastMessage('Recipe removed from Favorites');
-        setToastVariant(variants.info);
-      }
-    } catch (favoriteErr) {
-      if (!recipeToUpdate.favorite) {
-        setToastMessage('Oops! Could not save recipe in Favorites, try again');
-      } else {
-        setToastMessage('Oops! Could not delete recipe from Favorites, try again');
-      }
-      setToastVariant(variants.error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Recipe/All/${user.email}`);
-        setRecipes(data);
-      } catch (err) {
-        setFetchError(err);
-      }
-      setLoadingState(false);
-    };
-    fetchData();
-  }, []);
-
-  if (loadingState) {
-    return (
-      <LoadingDisplay />
-    );
-  }
-
-  if (fetchError) {
-    return (
-      <NotFound />
-    );
-  }
-
-  return (
-    <Box sx={{
-      display: 'grid',
-      gap: 3,
-      gridTemplateColumns: 'repeat(3, 1fr)',
-      marginBottom: 5,
-    }}
-    >
-      {recipes.map(({
-        id, name, category, description, favorite, imageUrl,
-      }) => (
-        <Card
-          sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            p: 1.5,
-            borderRadius: 5,
-          }}
-          key={id}
-          xs={1}
-          raised
+const RecipeCard = ({
+  allRecipes,
+  handleFavoriteRecipe,
+  isDeleteOpen,
+  handleCancelDeleteDialog,
+  handleClickDeleteDialog,
+  handleDeleteRecipe,
+}) => (
+  <Box sx={{
+    display: 'grid',
+    gap: 3,
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    marginBottom: 5,
+  }}
+  >
+    {allRecipes.map(({
+      id, name, category, description, favorite, imageUrl,
+    }) => (
+      <Card
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          p: 1.5,
+          borderRadius: 5,
+        }}
+        key={id}
+        xs={1}
+        raised
+      >
+        {imageUrl === null || imageUrl === '' ? (
+          <CardMedia
+            component="img"
+            image={SwitchImageCard(category)}
+            sx={{ width: 100, borderRadius: 2 }}
+            alt={`Picture of ${category}`}
+          />
+        ) : (
+          <CardMedia
+            component="img"
+            sx={{ width: 100, borderRadius: 2 }}
+            image={imageUrl}
+            alt={`Picture of ${category}`}
+          />
+        )}
+        <Box sx={{
+          display: 'flex', width: '70%', flexDirection: 'column',
+        }}
         >
-          {open && <DeleteDialogBox onCancel={handleCancel} onDelete={() => handleDelete(id)} />}
-          {imageUrl === null || imageUrl === '' ? (
-            <CardMedia
-              component="img"
-              image={SwitchImageCard(category)}
-              sx={{ width: 100, borderRadius: 2 }}
-              alt={`Picture of ${category}`}
-            />
-          ) : (
-            <CardMedia
-              component="img"
-              sx={{ width: 100, borderRadius: 2 }}
-              image={imageUrl}
-              alt={`Picture of ${category}`}
-            />
-          )}
+          <CardContent className="card-view-text line-clamp" sx={{ flex: '1 0 auto' }}>
+            <Link to={`/recipe/${id}`}>
+              <Typography variant="subtitle1" color="text.primary" component="div">
+                {name}
+              </Typography>
+            </Link>
+            <Typography variant="button" color="text.secondary" component="div">
+              {category}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
           <Box sx={{
-            display: 'flex', width: '70%', flexDirection: 'column',
+            display: 'flex', alignItems: 'center', p: 1, justifyContent: 'flex-end',
           }}
           >
-            <CardContent className="card-view-text line-clamp" sx={{ flex: '1 0 auto' }}>
-              <Link to={`/recipe/${id}`}>
-                <Typography variant="subtitle1" color="text.primary" component="div">
-                  {name}
-                </Typography>
-              </Link>
-              <Typography variant="button" color="text.secondary" component="div">
-                {category}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {description}
-              </Typography>
-            </CardContent>
-            <Box sx={{
-              display: 'flex', alignItems: 'center', p: 1, justifyContent: 'flex-end',
-            }}
-            >
-              <Tooltip title="Favorite">
-                <IconButton aria-label="favorite" onClick={() => handleClickFavorite(id)} data-testid="favorite icon" size="small">
-                  {favorite === true
-                    ? <FavoriteIcon sx={{ color: red[400], fontSize: 20 }} />
-                    : <FavoriteIcon sx={{ fontSize: 20 }} /> }
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Delete">
-                <IconButton aria-label="delete" onClick={handleClickOpen} size="small">
-                  <DeleteIcon sx={{ fontSize: 20 }} />
-                </IconButton>
-              </Tooltip>
-            </Box>
+            {isDeleteOpen && (
+            <DeleteDialogBox
+              id={id}
+              onCancel={handleCancelDeleteDialog}
+              onDelete={() => handleDeleteRecipe(id)}
+            />
+            )}
+            <Tooltip title="Favorite">
+              <IconButton aria-label="favorite" onClick={() => handleFavoriteRecipe(id)} data-testid="favorite icon" size="small">
+                {favorite === true
+                  ? <FavoriteIcon sx={{ color: red[400], fontSize: 20 }} />
+                  : <FavoriteIcon sx={{ fontSize: 20 }} /> }
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Delete">
+              <IconButton aria-label="delete" onClick={handleClickDeleteDialog} size="small">
+                <DeleteIcon sx={{ fontSize: 20 }} />
+              </IconButton>
+            </Tooltip>
           </Box>
-        </Card>
-      ))}
-      <Toast
-        onClose={() => setToastMessage('')}
-        message={toastMessage}
-        variant={toastVariant}
-      />
-    </Box>
-  );
+        </Box>
+      </Card>
+    ))}
+  </Box>
+);
+
+RecipeCard.defaultProps = {
+  allRecipes: [],
+  handleFavoriteRecipe: undefined,
+  isDeleteOpen: undefined,
+  handleClickDeleteDialog: undefined,
+  handleCancelDeleteDialog: undefined,
+  handleDeleteRecipe: undefined,
+};
+
+RecipeCard.propTypes = {
+  allRecipes: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    category: PropTypes.string,
+    description: PropTypes.string,
+    favorite: PropTypes.bool,
+    imageUrl: PropTypes.string,
+  })),
+  handleFavoriteRecipe: PropTypes.func,
+  isDeleteOpen: PropTypes.bool,
+  handleClickDeleteDialog: PropTypes.func,
+  handleCancelDeleteDialog: PropTypes.func,
+  handleDeleteRecipe: PropTypes.func,
 };
 
 export default RecipeCard;
--- web/src/components/recipe-list/list-view/ListView.jsx
+++ web/src/components/recipe-list/list-view/ListView.jsx
@@ -1,7 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import PropTypes from 'prop-types';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -11,158 +10,106 @@ import Avatar from '@mui/material/Avatar';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Typography from '@mui/material/Typography';
-import {
-  IconButton, Paper, Tooltip,
-} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import Tooltip from '@mui/material/Tooltip';
 import { red } from '@mui/material/colors';
-import LoadingDisplay from '../../loading-display/LoadingDisplay';
 import DeleteDialogBox from '../../DeleteDialogBox';
 import SwitchImageCard from '../../SwitchImageCard';
-import Toast, { variants } from '../../toast/Toast';
-import NotFound from '../../error-msgs/NotFound';
 
-const RecipeListView = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [loadingState, setLoadingState] = useState(true);
-  const [fetchError, setFetchError] = useState(null);
-  const [toastMessage, setToastMessage] = useState('');
-  const [toastVariant, setToastVariant] = useState(variants.info);
-  const [open, setOpen] = useState(false);
-  const [favoriteToggle, setFavoriteToggle] = useState(false);
-  const { user } = useAuth0();
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => setOpen(false);
-
-  const handleDelete = (id) => {
-    setOpen(false);
-    const deleteData = async () => {
-      try {
-        const { data } = await axios.delete(`${process.env.REACT_APP_BASE_API}/api/Recipe/${id}`);
-        setRecipes(data);
-        setToastMessage('Recipe has been deleted');
-        setToastVariant(variants.info);
-      } catch (err) {
-        setToastMessage('Oops! Could not delete recipe, try again');
-        setToastVariant(variants.error);
-      }
-    };
-    deleteData();
-  };
-
-  const handleClickFavorite = async (id) => {
-    const recipeToUpdate = recipes.find((recipe) => recipe.id === id);
-    try {
-      setFavoriteToggle((toggle) => !toggle);
-      const request = { ...recipeToUpdate, favorite: favoriteToggle, userEmail: user.email };
-      const { data } = await axios.put(`${process.env.REACT_APP_BASE_API}/api/Recipe/${id}`, request);
-      setRecipes(data);
-      if (!recipeToUpdate.favorite) {
-        setToastMessage('Recipe saved in Favorites');
-        setToastVariant(variants.success);
-      } else {
-        setToastMessage('Recipe removed from Favorites');
-        setToastVariant(variants.info);
-      }
-    } catch (favoriteErr) {
-      if (!recipeToUpdate.favorite) {
-        setToastMessage('Oops! Could not save recipe in Favorites, try again');
-      } else {
-        setToastMessage('Oops! Could not delete recipe from Favorites, try again');
-      }
-      setToastVariant(variants.error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/Recipe/All/${user.email}`);
-        setRecipes(data);
-      } catch (err) {
-        setFetchError(err);
-      }
-      setLoadingState(false);
-    };
-    fetchData();
-  }, []);
-
-  if (loadingState) {
-    return (
-      <LoadingDisplay />
-    );
-  }
-
-  if (fetchError) {
-    return (
-      <NotFound />
-    );
-  }
-
-  return (
-    <Paper elevation={3} sx={{ paddingTop: 2, paddingBottom: 3, paddingRight: 2 }}>
-      {recipes.map(({
-        id, name, description, category, favorite, imageUrl,
-      }) => (
-        <List sx={{ width: '100%' }} key={id}>
-          {open && <DeleteDialogBox onCancel={handleCancel} onDelete={() => handleDelete(id)} />}
-          <ListItem alignItems="flex-start">
-            <Link to={`/recipe/${id}`}>
-              <ListItemAvatar>
-                {imageUrl === null || imageUrl === ''
-                  ? (
-                    <Avatar
-                      alt={name}
-                      src={SwitchImageCard(category)}
-                      sx={{ width: 56, height: 56 }}
-                    />
-                  )
-                  : (
-                    <Avatar alt={name} src={imageUrl} sx={{ width: 56, height: 56 }} />
-                  )}
-              </ListItemAvatar>
-            </Link>
-            <ListItemText
-              primary={<Link to={`/recipe/${id}`}>{name}</Link>}
-              sx={{ pl: 2 }}
-              secondary={(
-                <>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    {category}
-                  </Typography>
-                  {`    ${description}`}
-                </>
+const RecipeListView = ({
+  allRecipes,
+  handleFavoriteRecipe,
+  isDeleteOpen,
+  handleCancelDeleteDialog,
+  handleClickDeleteDialog,
+  handleDeleteRecipe,
+}) => (
+  <Paper elevation={3} sx={{ paddingTop: 2, paddingBottom: 3, paddingRight: 2 }}>
+    {allRecipes.map(({
+      id, name, description, category, favorite, imageUrl,
+    }) => (
+      <List sx={{ width: '100%' }} key={id}>
+        <ListItem alignItems="flex-start">
+          <Link to={`/recipe/${id}`}>
+            <ListItemAvatar>
+              {imageUrl === null || imageUrl === ''
+                ? (
+                  <Avatar
+                    alt={name}
+                    src={SwitchImageCard(category)}
+                    sx={{ width: 56, height: 56 }}
+                  />
+                )
+                : (
+                  <Avatar alt={name} src={imageUrl} sx={{ width: 56, height: 56 }} />
+                )}
+            </ListItemAvatar>
+          </Link>
+          <ListItemText
+            primary={<Link to={`/recipe/${id}`}>{name}</Link>}
+            sx={{ pl: 2 }}
+            secondary={(
+              <>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {category}
+                </Typography>
+                {`    ${description}`}
+              </>
           )}
-            />
-            <Tooltip title="Favorite">
-              <IconButton aria-label="favorite" onClick={() => handleClickFavorite(id)} data-testid="favorite icon">
-                {favorite === true ? <FavoriteIcon sx={{ color: red[400] }} /> : <FavoriteIcon /> }
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Delete">
-              <IconButton aria-label="delete" onClick={handleClickOpen}>
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </ListItem>
-          <Divider variant="inset" component="li" />
-        </List>
-      ))}
-      <Toast
-        onClose={() => setToastMessage('')}
-        message={toastMessage}
-        variant={toastVariant}
-      />
-    </Paper>
-  );
+          />
+          {isDeleteOpen && (
+          <DeleteDialogBox
+            onCancel={handleCancelDeleteDialog}
+            onDelete={() => handleDeleteRecipe(id)}
+          />
+          )}
+          <Tooltip title="Favorite">
+            <IconButton aria-label="favorite" onClick={() => handleFavoriteRecipe(id)} data-testid="favorite icon">
+              {favorite === true ? <FavoriteIcon sx={{ color: red[400] }} /> : <FavoriteIcon /> }
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Delete">
+
+            <IconButton aria-label="delete" onClick={handleClickDeleteDialog}>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </ListItem>
+        <Divider variant="inset" component="li" />
+      </List>
+    ))}
+  </Paper>
+);
+
+RecipeListView.defaultProps = {
+  allRecipes: [],
+  handleFavoriteRecipe: undefined,
+  isDeleteOpen: undefined,
+  handleClickDeleteDialog: undefined,
+  handleCancelDeleteDialog: undefined,
+  handleDeleteRecipe: undefined,
+};
+
+RecipeListView.propTypes = {
+  allRecipes: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    category: PropTypes.string,
+    description: PropTypes.string,
+    favorite: PropTypes.bool,
+    imageUrl: PropTypes.string,
+  })),
+  handleFavoriteRecipe: PropTypes.func,
+  isDeleteOpen: PropTypes.bool,
+  handleClickDeleteDialog: PropTypes.func,
+  handleCancelDeleteDialog: PropTypes.func,
+  handleDeleteRecipe: PropTypes.func,
 };
 
 export default RecipeListView;
