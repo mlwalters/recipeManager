e5ef8cc30a66e45778dc8e64dafd3355749dbaa2
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2023-03-10 17:28:45 -0600

    60 - Changed fetching and post from useEffect to react query

--- web/src/components/grocery-list/GroceryList.jsx
+++ web/src/components/grocery-list/GroceryList.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
@@ -14,16 +13,27 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 const GroceryList = ({ items }) => {
-  const newItems = items;
-  // const handleToggle = (value) => {
-  //   const [checked, setChecked] = useState(false);
-  //   setChecked((prevStatus) => !prevStatus);
-  //   console.log(checked);
-  // };
+  const [checked, setChecked] = useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+      // eslint-disable-next-line no-console
+      console.log(checked);
+    } else {
+      newChecked.splice(currentIndex, 1);
+      // eslint-disable-next-line no-console
+      console.log(checked);
+    }
+    setChecked(newChecked);
+  };
 
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {newItems.map(({ id, name, isChecked }) => (
+      {items.map(({ id, name }) => (
         // const labelId = `<checkbox-list-label-1>${id}</checkbox-list-label-1>`;
         <ListItem
           key={id}
@@ -31,22 +41,24 @@ const GroceryList = ({ items }) => {
             <IconButton edge="end" aria-label="comments">
               <CommentIcon />
             </IconButton>
-            )}
+          )}
           disablePadding
         >
-          <ListItemButton role={Button} onClick={handleToggle((prevStatus) => !prevStatus)} dense>
+          <ListItemButton role={Button} onClick={handleToggle(id)} dense>
             <ListItemIcon>
               <Checkbox
                 icon={<RadioButtonUncheckedIcon />}
                 checkedIcon={<CheckCircleIcon />}
                 edge="start"
-                checked={isChecked}
+                checked={checked.indexOf(id) !== -1}
                 tabIndex={-1}
                 disableRipple
                 inputProps={{ 'aria-labelledby': id }}
               />
             </ListItemIcon>
-            <ListItemText id={id} primary={name}>{name}</ListItemText>
+            <ListItemText id={id} primary={name}>
+              {name}
+            </ListItemText>
           </ListItemButton>
         </ListItem>
       ))}
@@ -70,3 +82,77 @@ GroceryList.propTypes = {
 };
 
 export default GroceryList;
+
+// import React, { useState } from 'react';
+// // import React from 'react';
+// import PropTypes from 'prop-types';
+// import Button from '@mui/material/Button';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import IconButton from '@mui/material/IconButton';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import CommentIcon from '@mui/icons-material/Comment';
+// import Checkbox from '@mui/material/Checkbox';
+// import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+// import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+// const GroceryList = ({ items }) => {
+//   const newItems = items;
+//   // const handleToggle = (value) => {
+//   //   const [checked, setChecked] = useState(false);
+//   //   setChecked((prevStatus) => !prevStatus);
+//   //   console.log(checked);
+//   // };
+
+//   return (
+//     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+//       {newItems.map(({ id, name, isChecked }) => (
+//         // const labelId = `<checkbox-list-label-1>${id}</checkbox-list-label-1>`;
+//         <ListItem
+//           key={id}
+//           secondaryAction={(
+//             <IconButton edge="end" aria-label="comments">
+//               <CommentIcon />
+//             </IconButton>
+//             )}
+//           disablePadding
+//         >
+//           <ListItemButton role={Button}
+// onClick={handleToggle((prevStatus) => !prevStatus)} dense>
+//             <ListItemIcon>
+//               <Checkbox
+//                 icon={<RadioButtonUncheckedIcon />}
+//                 checkedIcon={<CheckCircleIcon />}
+//                 edge="start"
+//                 checked={isChecked}
+//                 tabIndex={-1}
+//                 disableRipple
+//                 inputProps={{ 'aria-labelledby': id }}
+//               />
+//             </ListItemIcon>
+//             <ListItemText id={id} primary={name}>{name}</ListItemText>
+//           </ListItemButton>
+//         </ListItem>
+//       ))}
+//     </List>
+//   );
+// };
+
+// GroceryList.defaultProps = {
+//   items: [],
+// };
+
+// GroceryList.propTypes = {
+//   items: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number,
+//       name: PropTypes.string.isRequired,
+//       checked: PropTypes.bool,
+//       userEmail: PropTypes.string,
+//     }),
+//   ),
+// };
+
+// export default GroceryList;
--- web/src/components/grocery-list/GroceryListPage.jsx
+++ web/src/components/grocery-list/GroceryListPage.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import Box from '@mui/material/Box';
@@ -12,16 +14,31 @@ import LoadingDisplay from '../sharedComponents/LoadingDisplay';
 import BackToHomeBtn from '../sharedComponents/BackToHomeBtn';
 import Toast, { variants } from '../sharedComponents/toast/Toast';
 import NotFound from '../sharedComponents/error/NotFound';
+import ValidationError from '../sharedComponents/error/ValidationError';
 
 const GroceryListPage = () => {
-  const [groceryItems, setGroceryItems] = useState([]);
-  const [loadingState, setLoadingState] = useState(true);
-  const [fetchError, setFetchError] = useState(null);
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const { user } = useAuth0();
+  const queryClient = useQueryClient();
   const [saveItemError, setSaveItemError] = useState(null);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
   const [toastVariant, setToastVariant] = useState(variants.info);
-  const { user } = useAuth0();
+
+  const fetchGroceryItems = () => axios.get(
+    `${process.env.REACT_APP_BASE_API}/api/GroceryItems/All/${user.email}`,
+  );
+
+  const groceriesQuery = useQuery({
+    queryKey: ['grocery-items'],
+    queryFn: fetchGroceryItems,
+  });
+
+  const {
+    // error: fetchError,
+    isLoading,
+    isError,
+    data: groceryItems,
+  } = groceriesQuery;
 
   const handleCloseAddModal = () => {
     setIsAddModalOpen(false);
@@ -29,46 +46,77 @@ const GroceryListPage = () => {
   };
 
   const handleSubmitAddModal = async (item) => {
-    if (item.itemName.trim() === '') {
-      setSaveItemError('Item can not be empty');
-    } else if (groceryItems.find((i) => i.name === item.itemName)) {
-      setSaveItemError('Item is already in the list');
-    } else {
-      const request = item;
-      request.userEmail = user.email;
-      try {
-        const newGroceryItem = await axios.post(`${process.env.REACT_APP_BASE_API}/api/GroceryItems/Add`, request);
-        setGroceryItems([...groceryItems, newGroceryItem.data]);
-        setSaveItemError(null);
-        handleCloseAddModal();
-        setToastMessage('Added new item');
-        setToastVariant(variants.success);
-      } catch (err) {
-        setSaveItemError('Oops! Could not save item. Please try again.');
-      }
+    if (item.itemName.trim().toLowerCase() === '') {
+      throw new ValidationError('Item can not be empty'); // Should I just use Yup though and not throw an error
     }
+    if (groceryItems.data.find((i) => i.name === item.itemName.trim().toLowerCase())) {
+      throw new ValidationError('Item is already in the list');
+    }
+    const request = item;
+    request.userEmail = user.email;
+    await axios.post(`${process.env.REACT_APP_BASE_API}/api/GroceryItems/Add`, request);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(`${process.env.REACT_APP_BASE_API}/api/GroceryItems/All/${user.email}`);
-        setGroceryItems(data);
-      } catch (err) {
-        setFetchError(err);
-      }
-      setLoadingState(false);
-    };
-    fetchData();
-  }, []);
+  const addGroceryItemMutation = useMutation({
+    mutationFn: handleSubmitAddModal,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['grocery-items']);
+      handleCloseAddModal();
+      setToastMessage('Added new item');
+      setToastVariant(variants.success);
+    },
+    onError: (error) => {
+      setSaveItemError(error.message);
+    },
+  });
+  // const [groceryItems, setGroceryItems] = useState([]);
+  // const [loadingState, setLoadingState] = useState(true);
+  // const [fetchError] = useState(null);
 
-  if (loadingState) {
+  // const handleSubmitAddModal = async (item) => {
+  //   if (item.itemName.trim() === '') {
+  //     setSaveItemError('Item can not be empty');
+  //   } else if (groceryItems.find((i) => i.name === item.itemName)) {
+  //     setSaveItemError('Item is already in the list');
+  //   } else {
+  //     const request = item;
+  //     request.userEmail = user.email;
+  //     try {
+  //       const newGroceryItem =
+  // await axios.post(`${process.env.REACT_APP_BASE_API}/api/GroceryItems/Add`, request);
+  //       setGroceryItems([...groceryItems, newGroceryItem.data]);
+  //       setSaveItemError(null);
+  //       handleCloseAddModal();
+  //       setToastMessage('Added new item');
+  //       setToastVariant(variants.success);
+  //     } catch (err) {
+  //       setSaveItemError('Oops! Could not save item. Please try again.');
+  //     }
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const { data } =
+  // await axios.get(`${process.env.REACT_APP_BASE_API}/api/GroceryItems/All/${user.email}`);
+  //       setGroceryItems(data);
+  //     } catch (err) {
+  //       setFetchError(err);
+  //     }
+  //     setLoadingState(false);
+  //   };
+  //   fetchData();
+  // }, []);
+
+  // if (loadingState) {
+  if (isLoading) {
     return (
       <LoadingDisplay />
     );
   }
 
-  if (fetchError) {
+  if (isError) {
     return (
       <Container maxWidth="lg">
         <BackToHomeBtn />
@@ -87,31 +135,43 @@ const GroceryListPage = () => {
     <>
       <Container maxWidth="lg" sx={{ marginBottom: 10 }}>
         <BackToHomeBtn />
-        <Box sx={{
-          width: '100%', maxWidth: 500, paddingTop: 2, paddingBottom: 2,
-        }}
+        <Box
+          sx={{
+            width: '100%',
+            maxWidth: 500,
+            paddingTop: 2,
+            paddingBottom: 2,
+          }}
         >
           <div>
             Last rendered
-            {' '}
             {new Date().toLocaleTimeString()}
           </div>
           <Typography variant="h3">
-            Grocery List (
-            {groceryItems.length}
+            Grocery List
+            (
+            {groceryItems.data.length}
             )
           </Typography>
         </Box>
-        <Button variant="contained" startIcon={<AddIcon />} onClick={() => setIsAddModalOpen(true)}>Add an item</Button>
+        <Button
+          variant="contained"
+          startIcon={<AddIcon />}
+          onClick={() => setIsAddModalOpen(true)}
+        >
+          Add an item
+        </Button>
         {isAddModalOpen && (
           <AddGroceryItemForm
             saveError={saveItemError}
-            onSubmit={handleSubmitAddModal}
+            // onSubmit={handleSubmitAddModal}
+            onSubmit={addGroceryItemMutation.mutate}
             handleClose={handleCloseAddModal}
           />
         )}
         <GroceryList
-          items={groceryItems.sort((a, b) => a.name.localeCompare(b.name))}
+          // items={groceryItems.sort((a, b) => a.name.localeCompare(b.name))}
+          items={groceryItems.data}
         />
         <Toast
           onClose={() => setToastMessage('')}
